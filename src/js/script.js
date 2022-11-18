e69aab79b2ed4a8e7c71f66b192f0969586ffaad
AllNNBusy <Butek63rus@gmail.com>
2022-11-18 19:38:15 +0300

    4 day js practice / mediaSlder

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,48 @@
-$(document).ready(function(){
-   $('.carousel__inner').slick({
-      speed: 1200,
-      slidesToShow: 1,
-      adaptiveHeight: true,
-      prevArrow: '<button type="button" class="carousel_prev"><img src="icons/carousel__button_left.png"></button>',
-      nextArrow: '<button type="button" class="carousel_next"><img src="icons/carousel__button_right.png"></button>',
-      responsive: [
-         {
-            breakpoint: 992,
-            settings: {
-               dots: true,
-               arrows: false
-            }
-         }
-      ]
-    });
+// $(document).ready(function(){
+//    $('.carousel__inner').slick({
+//       speed: 1200,
+//       slidesToShow: 1,
+//       adaptiveHeight: true,
+//       prevArrow: '<button type="button" class="carousel_prev"><img src="icons/carousel__button_left.png"></button>',
+//       nextArrow: '<button type="button" class="carousel_next"><img src="icons/carousel__button_right.png"></button>',
+//       responsive: [
+//          {
+//             breakpoint: 992,
+//             settings: {
+//                dots: true,
+//                arrows: false
+//             }
+//          }
+//       ]
+//     });
+//  });
+
+const slider = tns({
+   container: '.carousel__inner',
+   items: 1,
+   slideBy: 'page',
+   autoplay: false,
+   controls: false,
+   nav: false,
+   autoplay: false,
+   autoplayButtonOutput: false,
+   navPosition: "bottom",
+   responsive: {
+      992: {
+         nav: false,
+         autoplay: false,
+      },
+      300: {
+         nav: true,
+         autoplay: true
+      }
+   }
+ });
+
+ document.querySelector('.carousel_prev').addEventListener('click', function () {
+   slider.goTo('prev');
+ });
+
+ document.querySelector('.carousel_next').addEventListener('click', function () {
+   slider.goTo('next');
  });
