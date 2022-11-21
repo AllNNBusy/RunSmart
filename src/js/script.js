256c82683badd01bf5d1228a02c917e05b43da6d
AllNNBusy <Butek63rus@gmail.com>
2022-11-21 19:56:31 +0300

    7 day footer/media

--- src/js/script.js
+++ src/js/script.js
@@ -82,4 +82,27 @@ $(document).ready(function(){
 
    toggleSlide('.catalog-item__link');
    toggleSlide('.catalog-item__back');
+
+   // Modal
+
+   $('[data-modal=consultation]').on('click', function() {
+      $('.overlay, #consultation').fadeIn();
+   });
+   $('.modal__close').on('click', function() {
+      $('.overlay, #consultation, #order, #thanks').fadeOut();
+   });
+
+   $('.button_catalog').each(function(i) {
+      $(this).on('click', function() {
+         $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+         $('.overlay, #order').fadeIn();
+      })
+   });
+   // $('.thanks_btm').on('click', function() {
+   //    $('.overlay, #consultation, #order').fadeOut();
+   // });
+   // $('.thanks_btm').on('click', function() {
+   //    $('.overlay, #thanks').fadeIn();
+   // });
 });
+
